58314d2a8364cb4ede03ecca58ae4ab36360b727
RMaple <qiudanfeng1995@gmail.com>
2017-10-22 23:35:39 +0800

    [节点]PSensor的适配器已完成

--- app/src/main/java/com/ckt/d22400/sensortest_lp/ui/LSensorTestActivity.java
+++ app/src/main/java/com/ckt/d22400/sensortest_lp/ui/LSensorTestActivity.java
@@ -5,11 +5,14 @@ import android.os.Bundle;
 
 import com.ckt.d22400.sensortest_lp.R;
 
+import butterknife.ButterKnife;
+
 public class LSensorTestActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lsensor_test);
+        ButterKnife.bind(this);
     }
 }
